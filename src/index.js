f0be8f12fda66d0860f698929b9e1f90f52b0a14
Steve Hui <stevexu01@gmail.com>
2020-09-12 22:54:07 -0400

    Use ref to access input

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+//const url = 'http://localhost:8080/actuator/metrics';
+//const url = 'http://localhost:8080/points';
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+
+  async function callFetch(url) {
+    await fetch(url
+      , {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+      ).then(resp => resp.json()).then(data => console.log(data))
+    .catch(err => console.error(err))
+  }
+  
+  function getClaims() {
+    console.log("Axios call to backend url: ", urlRef.current.value)
+    fetch(urlRef.current.value).then(resp => resp.json()).then(
+        data => console.log(data)
+      );
+  
+    // callFetch(urlRef);
+  }
+  
+  function getClaimsCors(url) {
+    console.log("Axios call to backend API.")
+    fetch(url
+      // , {
+      //   mode: 'cors',
+      //   headers: {
+      //     'Access-Control-Allow-Origin': '*'
+      //   }
+      // }
+      ).then(resp => resp.json()).then(
+        data => console.log(data)
+      );
+  }
+
+  const urlRef = useRef();
+
+  return (
+    <div>
+      <Welcome name="Claim Viewer" />
+      <input ref={urlRef} name="url"></input>
+      <button name="send" onClick={ getClaims }>Get Claims</button>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
